76fb75cd3e40318104f193a286e20dfcc8d6b34f
Sébastien Tachier <sebastien.tachier@sebundefined.fr>
2020-05-03 18:00:34 +0200

    Adding signup feature with hashing. Adding mongo pod as well for storing user in user MS

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -9,17 +10,25 @@ router.post('/api/users/signup',[
     body('email').isEmail().withMessage("Email must be a valid email"),
     body('password').trim().isLength({ min: 4, max: 40 }).withMessage("Password must be between 4 and 40 characteres")
 ], 
-(req: Request, res: Response) => {
+async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array())
+        throw new RequestValidationError(errors.array());
     }
+    
     const { email, password } = req.body;
-    console.log('Creating a user');
-    throw new DatabaseConnectionError()
-    res.send({});
 
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser) {
+        throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
