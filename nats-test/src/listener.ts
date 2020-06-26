55cd9b0f4b49309746042a66c1b1f1bd33ccd948
Sébastien Tachier <sebastien.tachier@sebundefined.fr>
2020-06-26 12:07:27 +0200

    finish module NATS

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -16,10 +16,19 @@ stan.on('connect', () => {
         process.exit();
     });
     // setManualAckMode we have to run some processing to inform NATS that everything is fine. Otherwise NATS will send it back 30 sec later
-    const options = stan.subscriptionOptions().setManualAckMode(true);
+    const options = stan.subscriptionOptions()
+    // setManualAckMode we have to run some processing to inform NATS that everything is fine. Otherwise NATS will send it back 30 sec later
+    .setManualAckMode(true)
+    // setDeliverAllAvailable get the event saved by NATS previously. Cool but can be messy after weeks or month of use :)
+    .setDeliverAllAvailable()
+    // setDurableName make sure we do not send same message several time to same service. As sson as it's acked, the message will never be send again
+    .setDurableName('account-service');
+
+
     const subscription = stan.subscribe(
         'ticket:created',
-        'order-service-queue-group',
+        // Queue group. Durable subscription is based on the durable name and the queue group. As soon as a queue group got the message, it's not sent again
+        'account-service-queue-group',
         options
     );
 
@@ -38,4 +47,4 @@ stan.on('connect', () => {
 // Action à effectuer avant de terminer le process, en gros, informer que nous ne sommes plus en ligne au NATS
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-process.on('SIGTERM', () => stan.close());
+
